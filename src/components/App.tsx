631076e7287a9fea2baedc62d412d7a03b1754d4
Tu Nguyen <tuan@kms-technology.com>
2021-08-05 12:03:23 +0700

    remove provider duplicate

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,26 +8,24 @@ import NotFound from './features/notfound/NotFound';
 const App = () => {
   return (
     <Provider store={store}>
-      <Provider store={store}>
-        <HashRouter>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Switch>
-              {Routes.map((item) => {
-                const Component = item.component;
-                return (
-                  <Route
-                    key={item.path}
-                    path={item.path}
-                    exact
-                    render={(routeProps) => <Component {...routeProps} />}
-                  />
-                );
-              })}
-              <Route component={NotFound} />
-            </Switch>
-          </Suspense>
-        </HashRouter>
-      </Provider>
+      <HashRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            {Routes.map((item) => {
+              const Component = item.component;
+              return (
+                <Route
+                  key={item.path}
+                  path={item.path}
+                  exact
+                  render={(routeProps) => <Component {...routeProps} />}
+                />
+              );
+            })}
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </HashRouter>
     </Provider>
   );
 };
