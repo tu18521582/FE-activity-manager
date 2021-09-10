439127f81edc7e268909bef1a34e31321d9ef533
Tu Nguyen <tuan@kms-technology.com>
2021-09-10 14:38:20 +0700

    integrate backend to login

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,11 @@
 import { requests } from './api';
 
 const userService = {
-  login: (user: any) => requests.post('/user/login', user),
+  login: (user: any) =>
+    requests.post(
+      `/user/login?email=${user.email}&password=${user.password}`,
+      user
+    ),
   signup: (user: any) => {
     return requests.post('/user', user);
   },
