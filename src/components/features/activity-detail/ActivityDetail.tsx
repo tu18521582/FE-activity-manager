677d83f83d65aa4aa5c887a1a98ac1b0aaebb946
Tu Nguyen <tuan@kms-technology.com>
2021-09-22 09:46:56 +0700

    replace line code

--- src/components/features/activity-detail/ActivityDetail.tsx
+++ src/components/features/activity-detail/ActivityDetail.tsx
@@ -40,7 +40,7 @@ class ActivityDetail extends Component<ActivityDetailProps> {
           </div>
 
           <div className='activity-detail__btn-event'>
-            {this.props.activityIsViewing.host.id === this.props.userInfo.id ? (
+            {this.props.isLoggingUserHost ? (
               <div className='activity-detail__btn-manage'>
                 <Button type='primary' onClick={this.props.onClickButtonManage}>
                   Manage Event
